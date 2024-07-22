ed01170b2b5f9151558d2d3a856fa6990f684d58
Lucas Nicollier <lucas.nicollier@gmail.com>
2024-07-22 18:56:48 +0200

    formatted raycasting and cub.h

--- src/cub.h
+++ src/cub.h
@@ -1,26 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnicolli <lucas.nicollier@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 18:54:52 by lnicolli          #+#    #+#             */
+/*   Updated: 2024/07/22 18:54:54 by lnicolli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB_H
 # define CUB_H
 # include "hud.h"
 # include "keys.h"
 # include "parsing.h"
-# include <float.h>
 # include <math.h>
 # include <mlx.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/time.h>
-# include <math.h>
-# include <float.h>
 
 # ifndef M_PI
 #  define M_PI 3.14159265358979323846
 # endif
 
-# define PL printf("line: %d file: %s\n", __LINE__, __FILE__);
 # define ROTATION_SENSITIVITY 0.004
 
 # define AK 0
 # define AWP 1
+# define DBL_MAX 1.7976931348623158e+308
 
 # define SCREEN_W 1500
 # define SCREEN_H 1200
@@ -32,169 +41,180 @@ typedef struct s_time
 	unsigned long	elapsed_time;
 }					t_time;
 
-typedef struct s_mouse {
-    int			delta_x;
-	int			center_x;
-	int			center_y;
-	double		rotation_angle;
-	double		old_dir_x;
-	double		old_plane_x;
-}               t_mouse;
+typedef struct s_mouse
+{
+	int				delta_x;
+	int				center_x;
+	int				center_y;
+	double			rotation_angle;
+	double			old_dir_x;
+	double			old_plane_x;
+}					t_mouse;
 
 typedef struct s_data
 {
-	void		*img;
-	char		*addr;
-	int			bits_per_pixel;
-	int			line_length;
-	int			endian;
-}				t_data;
+	void			*img;
+	char			*addr;
+	int				bits_per_pixel;
+	int				line_length;
+	int				endian;
+}					t_data;
 
 typedef struct s_draw
 {
-	int			x;
-	int			y;
-	int			color;
-}				t_draw;
+	int				x;
+	int				y;
+	int				color;
+}					t_draw;
 
 typedef struct s_file
 {
-	int			fd;
-	char		**file;
-	int			lines_count;
-	char		*path;
-}				t_file;
+	int				fd;
+	char			**file;
+	int				lines_count;
+	char			*path;
+}					t_file;
 
 typedef struct s_screen
 {
-	void		*mlx;
-	void		*mlx_win;
-	int			width;
-	int			height;
-	t_data		img;
-	double		fov;
-	int			correction;
-}				t_screen;
+	void			*mlx;
+	void			*mlx_win;
+	int				width;
+	int				height;
+	t_data			img;
+	double			fov;
+	int				correction;
+}					t_screen;
 
 typedef struct s_player
 {
-	double		pos_x;
-	double		pos_y;
-	double		dir_x;
-	double		dir_y;
-	double		plane_x;
-	double		plane_y;
-}				t_player;
+	double			pos_x;
+	double			pos_y;
+	double			dir_x;
+	double			dir_y;
+	double			plane_x;
+	double			plane_y;
+}					t_player;
 
 typedef struct s_texture
 {
-	int			height;
-	int			width;
-	char		*pix;
-	void		*img;
-	int			bits_per_pixel;
-	int			size_line;
-	int			endian;
-}				t_texture;
+	int				height;
+	int				width;
+	char			*pix;
+	void			*img;
+	int				bits_per_pixel;
+	int				size_line;
+	int				endian;
+}					t_texture;
 
 typedef struct s_weapon
 {
-	int			selected_weapon;
+	int				selected_weapon;
 
-	t_texture	ak[WEAPON_FRAMES];
-	t_texture	awp[WEAPON_FRAMES];
+	t_texture		ak[WEAPON_FRAMES];
+	t_texture		awp[WEAPON_FRAMES];
 
-	int			frame;
-	int			is_shooting;
-	int			is_aiming;
-	int			can_shoot;
-}				t_weapon;
+	int				frame;
+	int				is_shooting;
+	int				is_aiming;
+	int				can_shoot;
+}					t_weapon;
 
 typedef struct s_score
 {
-	int			score;
+	int				score;
 
-	int			unit;
-	int			ten;
-	int			hundred;
+	int				unit;
+	int				ten;
+	int				hundred;
 
-	t_texture	t[10];
-}				t_score;
+	t_texture		t[10];
+}					t_score;
 
 typedef struct s_all
 {
-	t_map		m;
-	t_screen	s;
-	t_player	p;
-	t_texture	t[4];
-	t_weapon	w;
-	int			w_anim;
-	double		z_buffer[SCREEN_W];
+	t_map			m;
+	t_screen		s;
+	t_player		p;
+	t_texture		t[4];
+	t_weapon		w;
+	int				w_anim;
+	double			z_buffer[SCREEN_W];
 
-	t_score		score;
+	t_score			score;
 
-	int			frame;
-}				t_all;
+	int				frame;
+}					t_all;
 
 typedef struct s_ray
 {
-	double		ray_dir_x;
-	double		ray_dir_y;
-	double		delta_dist_x;
-	double		delta_dist_y;
-	int			step_x;
-	int			step_y;
-	double		side_dist_x;
-	double		side_dist_y;
-	int			side;
-	double		perp_wall_dist;
-	double		wall_x;
-	int			line_height;
-	int			draw_start;
-	int			draw_end;
-	int			hit_direction;
-}				t_ray;
+	double			ray_dir_x;
+	double			ray_dir_y;
+	double			delta_dist_x;
+	double			delta_dist_y;
+	int				step_x;
+	int				step_y;
+	double			side_dist_x;
+	double			side_dist_y;
+	int				side;
+	double			perp_wall_dist;
+	double			wall_x;
+	int				line_height;
+	int				draw_start;
+	int				draw_end;
+	int				hit_direction;
+	int				tex_x;
+	int				map_x;
+	int				map_y;
+}					t_ray;
 
 typedef struct s_object
 {
-    int sprite_screen_x;
-    int sprite_radius;
-    int draw_start_y;
-    int draw_end_y;
-    int draw_start_x;
-    int draw_end_x;
-    float sprite_x;
-    float sprite_y;
-    float transform_x;
-    float transform_y;
-} t_object;
+	int				sprite_screen_x;
+	int				sprite_radius;
+	int				draw_start_y;
+	int				draw_end_y;
+	int				draw_start_x;
+	int				draw_end_x;
+	float			sprite_x;
+	float			sprite_y;
+	float			transform_x;
+	float			transform_y;
+}					t_object;
 
-int				parse_mapfile(int ac, char *mapfile, t_all *a);
-char			get_pdir(t_all *a);
+int					parse_mapfile(int ac, char *mapfile, t_all *a);
+char				get_pdir(t_all *a);
 
-int				argb(unsigned char a, unsigned char b, unsigned char c,
-					unsigned char d);
-int				draw_screen(t_all *a);
-void			my_mlx_pixel_put(t_data *data, int x, int y, int color);
+int					argb(unsigned char a, unsigned char b, unsigned char c,
+						unsigned char d);
+int					draw_screen(t_all *a);
+void				my_mlx_pixel_put(t_data *data, int x, int y, int color);
 // minimap.c
-void			draw_minimap(t_all *a);
+void				draw_minimap(t_all *a);
 
-//objects.c
-void draw_objects(t_all *a);
+// raycasting_utils.c
+
+void				draw_tex_columm(int col, t_all *a, int i_tex, t_ray *ray);
+void				calculate_ray_pos_and_dir(t_all *a, int x, t_ray *ray);
+void				initialize_dda(t_all *a, t_ray *ray);
+void				calculate_step_and_side_dist(t_all *a, t_ray *ray);
+
+// objects.c
+void				draw_objects(t_all *a);
 // draw.c
-void			draw_square(t_draw d, int size, int color, t_all *a);
-void			draw_line(t_all *a, t_line_params *params);
+void				draw_square(t_draw d, int size, int color, t_all *a);
+void				draw_line(t_all *a, t_line_params *params);
 
 // hud
-void			draw_crosshair(t_all *a);
-void			draw_weapon_frame(t_all *a, int frame, t_texture *weapon);
-void			draw_points(t_all *a);
-void			draw_weapon(t_all *a);
-void			draw_digit(t_all *a, int pos_x, int digit);
-void			draw_score(t_all *a);
+void				draw_crosshair(t_all *a);
+void				draw_weapon_frame(t_all *a, int frame, t_texture *weapon);
+void				draw_points(t_all *a);
+void				draw_weapon(t_all *a);
+void				draw_digit(t_all *a, int pos_x, int digit);
+void				draw_score(t_all *a);
 // score
 void				parse_score(int score, t_all *a);
 
-double			calculate_fov(int degrees);
+double				calculate_fov(int degrees);
 
 #endif
--- src/raycasting.c
+++ src/raycasting.c
@@ -3,203 +3,107 @@
 /*                                                        :::      ::::::::   */
 /*   raycasting.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lnicolli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lnicolli <lucas.nicollier@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/22 17:45:49 by lnicolli          #+#    #+#             */
-/*   Updated: 2024/07/22 17:45:52 by lnicolli         ###   ########.fr       */
+/*   Created: 2024/07/22 18:38:37 by lnicolli          #+#    #+#             */
+/*   Updated: 2024/07/22 18:38:40 by lnicolli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-void draw_tex_columm(int column, int start, int end, t_all *a, int texX, int tex_index, t_ray *ray)
+void	perform_dda(t_all *a, t_ray *ray)
 {
-    double ratio = (float)a->t[tex_index].height / (float)(ray->line_height);
-    int offset = 0;
-    if (ray->line_height > a->s.height)
-        offset = ((double)(ray->line_height - a->s.height) / (double)ray->line_height) / 2.0 * a->t[tex_index].height;
+	int	hit;
 
-    int tex_color;
-    int i = 0;
-    int pos = 0;
-    while (i < start)
-        my_mlx_pixel_put(&a->s.img, column, i++, a->m.c_color & 0x00FFFFFF);
-    while (i < end)
-    {
-        pos = ((float)(i - start) * ratio) + offset;
-        pos = (int)round(pos * a->t[tex_index].size_line + texX * (float)(a->t[tex_index].bits_per_pixel / (float)8));
-        tex_color = ((int)a->t[tex_index].pix[pos] & 0x00FFFFFF);
-        my_mlx_pixel_put(&a->s.img, column, i++, tex_color);
-    }
-    while (end < SCREEN_H)
-        my_mlx_pixel_put(&a->s.img, column, end++, a->m.f_color & 0x00FFFFFF);
+	hit = 0;
+	while (hit == 0)
+	{
+		if (ray->side_dist_x < ray->side_dist_y)
+		{
+			ray->side_dist_x += ray->delta_dist_x;
+			ray->map_x += ray->step_x;
+			ray->side = 0;
+			if (ray->step_x > 0)
+				ray->hit_direction = 2;
+			else
+				ray->hit_direction = 3;
+		}
+		else
+		{
+			ray->side_dist_y += ray->delta_dist_y;
+			ray->map_y += ray->step_y;
+			ray->side = 1;
+			ray->hit_direction = (ray->step_y > 0);
+		}
+		hit = (a->m.map[ray->map_x][ray->map_y] == 1);
+	}
 }
 
-void calculate_ray_pos_and_dir(t_all *a, int x, t_ray *ray)
+void	calculate_wall_distance_and_texture(t_all *a, t_ray *ray)
 {
-    double camera_x;
-	camera_x = (2 * x / (double)SCREEN_W - 1) * a->s.fov;
-    ray->ray_dir_x = a->p.dir_x + a->p.plane_x * camera_x;
-    ray->ray_dir_y = a->p.dir_y + a->p.plane_y * camera_x;
+	if (ray->side == 0)
+		ray->perp_wall_dist = (ray->side_dist_x - ray->delta_dist_x);
+	else
+		ray->perp_wall_dist = (ray->side_dist_y - ray->delta_dist_y);
+	if (ray->side == 0)
+		ray->wall_x = a->p.pos_y + ray->perp_wall_dist * ray->ray_dir_y;
+	else
+		ray->wall_x = a->p.pos_x + ray->perp_wall_dist * ray->ray_dir_x;
+	ray->wall_x -= floor(ray->wall_x);
 }
 
-void initialize_dda(t_all *a, t_ray *ray, int *map_x, int *map_y)
+void	calculate_line_height_and_draw_points(t_ray *ray, t_all *a)
 {
-    *map_x = (int)(a->p.pos_x);
-    *map_y = (int)(a->p.pos_y);
+	double	h;
 
-    if (ray->ray_dir_x == 0)
-        ray->delta_dist_x = DBL_MAX;
-    else
-        ray->delta_dist_x = fabs(1.0 / ray->ray_dir_x);
-
-    if (ray->ray_dir_y == 0)
-        ray->delta_dist_y = DBL_MAX;
-    else
-        ray->delta_dist_y = fabs(1.0 / ray->ray_dir_y);
-}
-
-void calculate_step_and_side_dist(t_all *a, t_ray *ray, int map_x, int map_y)
-{
-    if (ray->ray_dir_x < 0)
-    {
-        ray->step_x = -1;
-        ray->side_dist_x = (a->p.pos_x - map_x) * ray->delta_dist_x;
-    }
-    else
-    {
-        ray->step_x = 1;
-        ray->side_dist_x = (map_x + 1.0 - a->p.pos_x) * ray->delta_dist_x;
-    }
-    if (ray->ray_dir_y < 0)
-    {
-        ray->step_y = -1;
-        ray->side_dist_y = (a->p.pos_y - map_y) * ray->delta_dist_y;
-    }
-    else
-    {
-        ray->step_y = 1;
-        ray->side_dist_y = (map_y + 1.0 - a->p.pos_y) * ray->delta_dist_y;
-    }
-}
-
-void perform_dda(t_all *a, t_ray *ray, int *map_x, int *map_y, int *hit)
-{
-    while (*hit == 0)
-    {
-        if (ray->side_dist_x < ray->side_dist_y)
-        {
-            ray->side_dist_x += ray->delta_dist_x;
-            *map_x += ray->step_x;
-            ray->side = 0;
-            if (ray->step_x > 0)
-                ray->hit_direction = 2;
-            else
-                ray->hit_direction = 3;
-        }
-        else
-        {
-            ray->side_dist_y += ray->delta_dist_y;
-            *map_y += ray->step_y;
-            ray->side = 1;
-            if (ray->step_y > 0)
-                ray->hit_direction = 1;
-            else
-                ray->hit_direction = 0;
-        }
-        if (a->m.map[*map_x][*map_y] == 1)
-            *hit = 1;
-    }
-}
-
-void calculate_wall_distance_and_texture(t_all *a, t_ray *ray)
-{
-    if (ray->side == 0)
-        ray->perp_wall_dist = (ray->side_dist_x - ray->delta_dist_x);
-    else
-        ray->perp_wall_dist = (ray->side_dist_y - ray->delta_dist_y);
-
-    if (ray->side == 0)
-        ray->wall_x = a->p.pos_y + ray->perp_wall_dist * ray->ray_dir_y;
-    else
-        ray->wall_x = a->p.pos_x + ray->perp_wall_dist * ray->ray_dir_x;
-    ray->wall_x -= floor(ray->wall_x);
-}
-
-void calculate_line_height_and_draw_points(t_ray *ray, t_all *a)
-{
-    double h;
 	h = SCREEN_H;
-    ray->line_height = (int)round(h / ray->perp_wall_dist) * a->s.correction;
-    ray->draw_start = -(ray->line_height) / 2 + h / 2;
-    if (ray->draw_start < 0)
-        ray->draw_start = 0;
-    ray->draw_end = (ray->line_height) / 2 + h / 2;
-    if (ray->draw_end >= h)
-        ray->draw_end = h - 1;
+	ray->line_height = (int)round(h / ray->perp_wall_dist) * a->s.correction;
+	ray->draw_start = -(ray->line_height) / 2 + h / 2;
+	if (ray->draw_start < 0)
+		ray->draw_start = 0;
+	ray->draw_end = (ray->line_height) / 2 + h / 2;
+	if (ray->draw_end >= h)
+		ray->draw_end = h - 1;
 }
 
-
-
-int draw_screen(t_all *a)
+void	raycasting(t_all *a, t_ray *ray, int x)
 {
-    int x = 0;
-    while (x < SCREEN_W)
-    {
-        t_ray ray;
-        int map_x, map_y;
-        int hit = 0;
+	int	tex_index;
 
-        calculate_ray_pos_and_dir(a, x, &ray);
-        initialize_dda(a, &ray, &map_x, &map_y);
-        calculate_step_and_side_dist(a, &ray, map_x, map_y);
-        perform_dda(a, &ray, &map_x, &map_y, &hit);
-        calculate_wall_distance_and_texture(a, &ray);
-        calculate_line_height_and_draw_points(&ray, a);
+	calculate_ray_pos_and_dir(a, x, ray);
+	initialize_dda(a, ray);
+	calculate_step_and_side_dist(a, ray);
+	perform_dda(a, ray);
+	calculate_wall_distance_and_texture(a, ray);
+	calculate_line_height_and_draw_points(ray, a);
+	ray->tex_x = (int)(ray->wall_x * a->t[0].width);
+	if (ray->side == 0 && ray->ray_dir_x > 0)
+		ray->tex_x = a->t[0].width - ray->tex_x - 1;
+	if (ray->side == 1 && ray->ray_dir_y < 0)
+		ray->tex_x = a->t[0].width - ray->tex_x - 1;
+	if (ray->side == 0)
+		tex_index = (int)(ray->ray_dir_x < 0) + 2;
+	else
+		tex_index = (int)((ray->ray_dir_y < 0));
+	draw_tex_columm(x, a, tex_index, ray);
+	a->z_buffer[x] = ray->perp_wall_dist;
+}
 
-        int tex_x = (int)(ray.wall_x * a->t[0].width);
-        if (ray.side == 0 && ray.ray_dir_x > 0) tex_x = a->t[0].width - tex_x - 1;
-        if (ray.side == 1 && ray.ray_dir_y < 0) tex_x = a->t[0].width - tex_x - 1;
+int	draw_screen(t_all *a)
+{
+	int		x;
+	t_ray	ray;
 
-        int tex_index;
-        if (ray.side == 0)
-        {
-            if (ray.ray_dir_x > 0)
-                tex_index = 2;  
-            else
-                tex_index = 3;  
-        }
-        else
-        {
-            if (ray.ray_dir_y > 0)
-                tex_index = 0;  
-            else
-                tex_index = 1;  
-        }
-
-        draw_tex_columm(x, ray.draw_start, ray.draw_end, a, tex_x, tex_index, &ray);
-
-        
-        a->z_buffer[x] = ray.perp_wall_dist;
-
-        x++;
-    }
-
-    
-    draw_objects(a);
-
-    
+	x = 0;
+	while (x < SCREEN_W)
+		raycasting(a, &ray, x++);
+	draw_objects(a);
 	a->s.correction = 1;
 	a->s.fov = calculate_fov(60) / a->s.correction;
-
-    draw_weapon(a);
-    draw_minimap(a);
-
-    draw_score(a);
-
-
-    mlx_put_image_to_window(a->s.mlx, a->s.mlx_win, a->s.img.img, 0, 0);
-
-    return 1;
+	draw_weapon(a);
+	draw_minimap(a);
+	draw_score(a);
+	mlx_put_image_to_window(a->s.mlx, a->s.mlx_win, a->s.img.img, 0, 0);
+	return (1);
 }
-
